dba5d2ca39d2502ff11078cd1e945daaabb9b074
hpknollenberg <hpknollenberg@gmail.com>
2024-06-05 15:43:11 -0400

    deployed and got rid of image model

---
+++ app_social_media/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    first_name = models.TextField()
+    last_name = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+    
+class Message(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, related_name="author_to_message_set")
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    likes = models.ManyToManyField(Profile, related_name="likes") 
---
+++ app_social_media/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import *
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(many=False)
+    class Meta:
+        model = Profile
+        fields = ['id', 'first_name', 'last_name', 'user']
+
+class MessageSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(many=False)
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S UTC")
+    likes_count = serializers.IntegerField(
+        source='likes.count',
+        read_only=True
+    )
+
+    class Meta:
+        model = Message
+        fields = ['id', 'author', 'created_at', 'content', 'image', 'likes_count']
